73108388e95153958ff2d4dd80f306c2f1493993
Logan Chien <tzuhsiang.chien@gmail.com>
2015-05-17 22:53:38 +0800

    Switch to compile-abi-with-cmake.sh by default.

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 source "${SCRIPT_DIR}/config.sh"
 
 "${SCRIPT_DIR}/checkout-repo.sh"
-"${SCRIPT_DIR}/compile-abi.sh"
+"${SCRIPT_DIR}/compile-libunwind.sh"
+"${SCRIPT_DIR}/compile-abi-with-cmake.sh"
 "${SCRIPT_DIR}/compile-libcxx.sh"
 "${SCRIPT_DIR}/compile-unittest.sh"
